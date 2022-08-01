2d1488da0ce4a109822d572d8dee94ae8f8abbec
loveLIUXX <sznzn614866450@gmail.com>
2022-08-01 08:49:57 +0800

    first commit

---
+++ src/main/java/com/github/wsjuanve/util/Base64Util.java
@@ -0,0 +1,12 @@
+package com.github.wsjuanve.util;/**
+ * Created by ChenHao on 2022/8/1 is 8:46.
+ * 
+ * @description
+ * @author  sznzn <sznzn614866450@outlook.com>
+ * @version V1.0.0
+ * @since 1.0
+ * @date 2022/8/1
+ */
+
+    public class Base64Util {
+}
