1ca0e39a6318538b17801c61d771ba92c0279c80
loveLIUXX <sznzn614866450@gmail.com>
2022-08-01 08:50:46 +0800

    dsafasd

--- src/main/java/com/github/wsjuanve/util/Base64Util.java
+++ src/main/java/com/github/wsjuanve/util/Base64Util.java
@@ -1,12 +1,56 @@
-package com.github.wsjuanve.util;/**
+package com.github.wsjuanve.util;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+/**
  * Created by ChenHao on 2022/8/1 is 8:46.
- * 
- * @description
- * @author  sznzn <sznzn614866450@outlook.com>
+ *
+ * @author sznzn
  * @version V1.0.0
  * @since 1.0
- * @date 2022/8/1
  */
 
-    public class Base64Util {
+public class Base64Util {
+
+
+    /**
+     * 将图片转换成Base64编码
+     *
+     * @param imgFile 待处理图片地址
+     * @return
+     */
+    public static String getImgBase(String imgFile) {
+
+        // 将图片文件转化为二进制流
+        InputStream in = null;
+        byte[] data = null;
+        // 读取图片字节数组
+        try {
+            in = Files.newInputStream(Paths.get(imgFile));
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Encoder encoder = Base64.getEncoder();
+        return encoder.encodeToString(data);
+    }
+
+
+    public static String getImgBaseWithHead(String imgFile) {
+        String imgBase = getImgBase(imgFile);
+        // 图片头
+        String imghead = "data:image/jpeg;base64,";
+        return imghead + imgBase;
+    }
+
+
 }
